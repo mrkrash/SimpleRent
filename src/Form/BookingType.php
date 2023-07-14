71d717d2c9abd039b359e1078d2ab0e931e5d44f
Mario Ravalli <mario@raval.li>
2023-07-15 00:50:45 +0200

    Updated booking crud

---
+++ src/Form/BookingType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateStart')
+            ->add('dateEnd')
+            ->add('products', CollectionType::class, [
+                'entry_type' => ChoiceType::class,
+                'entry_options' => [
+                    'choices' => $options['productList_choices'],
+                    'choice_label' => 'name',
+                ],
+                'allow_add' => true,
+            ])
+            ->add('notes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+            'productList_choices' => [],
+        ]);
+    }
+}
