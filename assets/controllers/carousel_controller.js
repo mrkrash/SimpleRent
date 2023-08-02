2598dffbaa74fed66da2ba317ab5d7df7ae17124
Mario Ravalli <mario@raval.li>
2023-08-03 00:55:25 +0200

    Added carousel on products main page

--- assets/controllers/carousel_controller.js
+++ assets/controllers/carousel_controller.js
@@ -1,14 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 import bulmaCarousel from "bulma-carousel/src/js";
 
-const options = {
-    autoplay: true,
-    infinite: true,
-    loop: true,
-};
 export default class extends Controller {
     connect() {
-        const carousels = bulmaCarousel.attach('.carousel', options);
-
+        bulmaCarousel.attach('.hero-carousel.carousel', {
+            autoplay: true,
+            infinite: true,
+            loop: true,
+        });
+        bulmaCarousel.attach('.product.carousel', {
+            autoplay: false,
+            infinite: true,
+            loop: true,
+            slidesToScroll: 4,
+            slidesToShow: 4
+        });
     }
 }
