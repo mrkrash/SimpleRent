9e2b07e0227d7c3c66e10837bf8e68916ccb93c4
Mario Ravalli <mario@raval.li>
2023-10-14 12:55:38 +0200

    Add command for create user

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'user:create',
+    description: 'Creates a new user.'
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly UserRepository $userRepository
+    ) {
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command allows you to create a user')
+            ->addArgument('username', InputArgument::REQUIRED, 'The username to use.')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password to use.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $username = $input->getArgument('username');
+        $password = $input->getArgument('password');
+
+        $output->writeln([
+            'User Creation',
+            '=============',
+            '',
+            'Username: ' . $username,
+        ]);
+
+        $user = (new User())->setUsername($username);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+        $this->userRepository->save($user, true);
+
+        $output->writeln('User successfully generated!');
+
+        return Command::SUCCESS;
+    }
+}
