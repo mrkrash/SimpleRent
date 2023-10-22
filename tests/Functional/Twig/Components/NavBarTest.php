f4285a37c4725b1109b82e41e63179458db237f8
Mario Ravalli <mario@raval.li>
2023-10-22 23:31:00 +0200

    Add Testsuite and refactored gitflow

---
+++ tests/Functional/Twig/Components/NavBarTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Functional\Twig\Components;
+
+use App\Twig\Components\NavBar;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Symfony\UX\TwigComponent\Test\InteractsWithTwigComponents;
+
+/**
+ * @coversNothing
+ */
+class NavBarTest extends KernelTestCase
+{
+    use InteractsWithTwigComponents;
+
+    public function testComponentMount(): void
+    {
+        $request = new Request();
+        $request->setSession(new Session(new MockArraySessionStorage()));
+
+        self::getContainer()->get(RequestStack::class)->push($request);
+
+        $component = $this->mountTwigComponent(
+            name: NavBar::class
+        );
+
+        self::assertInstanceOf(NavBar::class, $component);
+    }
+
+}
